8bcc72ccb2024b50fa7058254f4dccaa6a688a36
江舸 <kou@jianggedeMacBook-Air.local>
2018-08-27 16:10:30 +0800

    修改样式

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,8 @@ clipboard.on('success', function(e) {
         timer:1000,
         showConfirmButton:false
     });
-    console.log(e);
+    // console.log(e);
 });
 clipboard.on('error', function(e) {
-    console.log(e);
+    // console.log(e);
 });
-
-var arr = ['b' , 'a' , 'd' , 'c' , 'e'];
-console.log(arr.push(['x','s']));
-console.log(arr);
