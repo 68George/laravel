78c255ca85227a982796a70635c11f40b47be31e
George <17600905795@163.com>
2021-10-12 11:02:21 +0800

    Revert "修改样式"
    This reverts commit 8bcc72ccb2024b50fa7058254f4dccaa6a688a36.

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,12 @@ clipboard.on('success', function(e) {
         timer:1000,
         showConfirmButton:false
     });
-    // console.log(e);
+    console.log(e);
 });
 clipboard.on('error', function(e) {
-    // console.log(e);
+    console.log(e);
 });
+
+var arr = ['b' , 'a' , 'd' , 'c' , 'e'];
+console.log(arr.push(['x','s']));
+console.log(arr);
